e0df9f5625a7c791be8595a3d4f0a44c6acdf9f4
MarlonMejia15 <marlonmejiamonterroso@gmail.com>
2023-06-11 21:12:15 -0600

    Se realizo la conexion con SQL SERVER, y se comento el uso de memoria, se probo todos los metodos con SQL y correcto

---
+++ CarrosAPI/Migrations/20230612030657_Nueva Base de datos CarrosDb.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarrosAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class NuevaBasededatosCarrosDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Carros",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    modelo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    marca = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Gasolina = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    millaje = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carros", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Carros");
+        }
+    }
+}
--- CarrosAPI/Program.cs
+++ CarrosAPI/Program.cs
@@ -9,7 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<CarroAPIDbContext>(options => options.UseInMemoryDatabase("CarroDb"));
+//builder.Services.AddDbContext<CarroAPIDbContext>(options => options.UseInMemoryDatabase("CarroDb"));
+builder.Services.AddDbContext<CarroAPIDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("CarrosAPIGetConnectionString")));
 
 var app = builder.Build();
 
